0ec1c20b782049d7be643f063d50f5c38f3f33d4
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-23 15:16:46 +0300

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,57 @@
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-});
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = Number(event.target.elements.delay.value);
+  const stateRs = event.target.elements.state.value;
+
+  let state;
+
+  if (delay === 0) {
+    return;
+  }
+
+  if (stateRs === 'fulfilled') {
+    state = true;
+  } else if (stateRs === 'rejected') {
+    state = false;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === true) {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      iziToast.show({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${value}ms`,
+        titleColor: '#ffffff',
+        messageColor: '#ffffff',
+        backgroundColor: '#59a10d',
+        timeout: 8000,
+      });
+    })
+    .catch(error => {
+      iziToast.show({
+        title: 'Error',
+        message: `❌ Rejected promise in ${error}ms`,
+        titleColor: '#ffffff',
+        messageColor: '#ffffff',
+        backgroundColor: '#ef4040',
+        timeout: 8000,
+      });
+    });
+}
