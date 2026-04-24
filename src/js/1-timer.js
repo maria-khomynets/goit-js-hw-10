c60f0dd2617f69a84572219e2869021aa50c5e12
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-24 15:46:05 +0300

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 const datetimePicker = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 const dataDays = document.querySelector('[data-days]');
@@ -11,8 +12,10 @@ const dataSeconds = document.querySelector('[data-seconds]');
 
 startBtn.addEventListener('click', handleClick);
 startBtn.disabled = true;
+
 let selectedTime = null;
 let intervalId = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -20,9 +23,8 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const currentTime = Date.now();
-    console.log(currentTime);
     selectedTime = selectedDates[0].getTime();
-    console.log(selectedTime);
+
     if (selectedTime <= currentTime) {
       iziToast.show({
         title: 'Warning',
@@ -32,7 +34,6 @@ const options = {
         timeout: 8000,
       });
       startBtn.disabled = true;
-      return;
     } else {
       startBtn.disabled = false;
     }
@@ -44,39 +45,52 @@ flatpickr(datetimePicker, options);
 function handleClick() {
   startBtn.disabled = true;
   datetimePicker.disabled = true;
+
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+
   intervalId = setInterval(() => {
     const currentTime = Date.now();
     const deltaTime = selectedTime - currentTime;
+
     if (deltaTime <= 0) {
       clearInterval(intervalId);
 
+      dataDays.textContent = '00';
+      dataHours.textContent = '00';
+      dataMinutes.textContent = '00';
+      dataSeconds.textContent = '00';
+
       datetimePicker.disabled = false;
+      startBtn.disabled = true;
+
       return;
     }
+
     const objTime = convertMs(deltaTime);
 
-    dataDays.innerHTML = String(objTime.days).padStart(2, '0');
-    dataHours.innerHTML = String(objTime.hours).padStart(2, '0');
-    dataMinutes.innerHTML = String(objTime.minutes).padStart(2, '0');
-    dataSeconds.innerHTML = String(objTime.seconds).padStart(2, '0');
+    dataDays.textContent = addLeadingZero(objTime.days);
+    dataHours.textContent = addLeadingZero(objTime.hours);
+    dataMinutes.textContent = addLeadingZero(objTime.minutes);
+    dataSeconds.textContent = addLeadingZero(objTime.seconds);
   }, 1000);
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
