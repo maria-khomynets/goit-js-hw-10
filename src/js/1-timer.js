9b9b4678920c22bd6930fe0fafbf7a18979f62e6
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-24 13:27:44 +0300

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,22 +1,35 @@
-function checkPassword(password) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (password === 'admin123') {
-        resolve('ok');
-      } else {
-        reject('ooops');
-      }
-    }, 1000);
-  });
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const datetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+let selectedTime = null;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentTime = Date.now();
+    console.log(currentTime);
+    selectedTime = selectedDates[0].getTime();
+    console.log(selectedTime);
+    if (selectedTime <= currentTime) {
+      iziToast.show({
+        title: 'Warning',
+        message: 'Please choose a date in the future',
+        backgroundColor: 'pink',
+        position: 'topLeft',
+        timeout: 8000,
+      });
+      startBtn.disabled = true;
+      return;
+    } else (
+       startBtn.disabled = false; 
+    )
+  },
+};
 
-  //   data
-  //     .then(res => {
-  //       console.log(res);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-}
-checkPassword('admin123')
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+flatpickr(datetimePicker, options);
